659d523c9332c1e2430402c2d113facd60e2cda8
假富贵儿 <>
2019-11-16 21:28:55 +0800

    DemoInit

---
+++ MoyaPromiseKitDemo/MoyaPromiseKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoyaPromiseKitDemo
+//
+//  Created by 假富贵儿 on 2019/11/16.
+//  Copyright © 2019 假富贵儿. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
